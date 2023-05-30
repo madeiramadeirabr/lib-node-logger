2a2789a6f2cc929cea5a54b3eda47c480ca4d572
Lucas Block <lucas.blockv@gmail.com>
2023-05-30 13:34:06 -0300

    feat!: first release nodejs logger

---
+++ src/core/type/config.ts
@@ -0,0 +1,4 @@
+export type Config = {
+    level: string,
+    serviceName: string
+}
---
+++ src/core/type/log-message-options-type.ts
@@ -0,0 +1,6 @@
+export type LogMessageOptions = {
+    global_event_name?: string
+    context?: string
+    trace_id?: string
+    session_id?: string
+}
