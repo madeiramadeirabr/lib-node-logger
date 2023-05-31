469fb4a2572b496b64a65a9f612e7e68d319701d
Lucas Block <lucas.blockv@gmail.com>
2023-05-31 16:23:27 -0300

    chore: update mock

--- tests/mock/formatterMock.ts
+++ tests/mock/formatterMock.ts
@@ -1,10 +1,10 @@
 import { FormatterInterface } from '../../src/core/interface/formatter';
 import { LogMessageOptions } from '../../src/core/type/log-message-options-type';
-import { formatInfoResponse } from '../stubs/formatResponse';
+import * as formatResponseMock from '../stubs/formatResponse';
 
 export class FormatterMock implements FormatterInterface {
 
     format(message: string, level: string, options?: LogMessageOptions | undefined): string {
-        return formatInfoResponse;
+        return formatResponseMock.formatInfoResponse;
     }
 }
