69653fdb6702dd8c5b87e21824ee73489b6f93c2
Rafael Ortega Bueno <rorteg@gmail.com>
2024-09-03 18:00:17 -0300

    feat: update exports to include MMRFC5 standard event components

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { Container } from './container';
 export { Config } from './core/type/config';
 export { LogMessageOptions } from './core/type/log-message-options-type';
-export { LogLevel } from "./core/type/log-level";
+export { LogLevel } from './core/type/log-level';
+export { LogStandardGlobalEventNameEnum } from './core/type/log-standard-global-event-name.enum';
+export * from './core/type/log-standard-global-event-name-context-type';
